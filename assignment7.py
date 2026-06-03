7ad0c9e8417f44242eb06cdc63487e39f2a82fcf
Atharva Aiwale <atharva.aiwale28@gmail.com>
2026-06-03 13:20:51 +0530

    Create a script to combine three name strings
    Combine first, father, and last names into a single string.

---
+++ assignment7.py
@@ -0,0 +1,7 @@
+#7. Create three strings and then create a third variable combining the three original strings.
+
+first_name = "Atharva "
+father_name = "Vinod "
+last_name = "Aiwale"
+
+print(first_name + father_name + last_name)
